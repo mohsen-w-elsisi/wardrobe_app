b56ed6d13302b2888bd0da09f968fa42ab6d6ee0
mohsen-w-elsisi <mohsenelsisi@outlook.com>
2024-07-17 14:51:15 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# wardrobe_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/wardrobe_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wardrobe_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
