9d2f46f614e5bb70ee086f869b9b50aaeb4f3253
mohsen-w-elsisi <mohsenelsisi@outlook.com>
2024-08-13 21:30:21 +0300

    sharing outfits now works

--- lib/outfiting/outfit_presenter_screen.dart
+++ lib/outfiting/outfit_presenter_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:wardrobe_app/cloth_item/cloth_item.dart';
 import 'package:wardrobe_app/cloth_item_views/grid_view.dart';
 
@@ -12,12 +14,52 @@ class OutfitPresenterScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          const SliverAppBar.medium(
-            title: Text("Outfit"),
-          ),
+          _AppBar(clothItems: clothItems),
           ClothItemGridView(clothItems, sliver: true),
         ],
       ),
+    );
+  }
+}
+
+class _AppBar extends StatelessWidget {
+  final List<ClothItem> clothItems;
+
+  _AppBar({required this.clothItems});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar.medium(
+      actions: _actions,
+      title: const Text("Outfit"),
+    );
+  }
+
+  List<Widget> get _actions {
+    return [
+      _shareButton,
+      _saveButton,
+    ];
+  }
+
+  IconButton get _shareButton {
+    return IconButton(
+      tooltip: "share",
+      onPressed: _share,
+      icon: const Icon(Icons.share),
+    );
+  }
+
+  void _share() {
+    final xFiles = [for (final item in clothItems) XFile.fromData(item.image)];
+    Share.shareXFiles(xFiles, text: "what do you think of this outfit");
+  }
+
+  IconButton get _saveButton {
+    return IconButton(
+      tooltip: "save",
+      onPressed: () {},
+      icon: const Icon(Icons.save_alt),
     );
   }
 }
