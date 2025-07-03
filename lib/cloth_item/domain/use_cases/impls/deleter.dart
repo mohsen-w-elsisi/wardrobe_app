f459c2b9f6b0cf3900e1a139c584bf1bedb6bfc5
mohsen-w-elsisi <mohsenelsisi@outlook.com>
2025-07-03 14:40:07 +0300

    outfits containing items which were deleted are now also deleted

--- lib/cloth_item/domain/use_cases/impls/deleter.dart
+++ lib/cloth_item/domain/use_cases/impls/deleter.dart
@@ -1,5 +1,7 @@
+import 'package:get_it/get_it.dart';
 import 'package:wardrobe_app/cloth_item/domain/entities/data_structures.dart';
 import 'package:wardrobe_app/cloth_item/domain/use_cases/use_cases.dart';
+import 'package:wardrobe_app/outfit/domain/use_cases/use_cases.dart';
 
 import 'use_case_utils.dart';
 
@@ -7,12 +9,23 @@ class ClothItemDeleterImpl extends ClothItemDeleter with UseCaseUtils {
   @override
   void delete(ClothItem item) {
     dataGateway.delete(item.id);
+    _deleteOutfitsContaining(item);
     notifyUi();
+  }
+
+  void _deleteOutfitsContaining(ClothItem item) {
+    final allOutfits = GetIt.I<OutfitQuerier>().getAll();
+    for (final outfit in allOutfits) {
+      if (outfit.items.contains(item.id)) {
+        GetIt.I<OutfitDeleter>().delete(outfit);
+      }
+    }
   }
 
   @override
   void clearWardrobe() {
     dataGateway.deleteAll();
+    GetIt.I<OutfitDeleter>().deleteAll();
     notifyUi();
   }
 }
